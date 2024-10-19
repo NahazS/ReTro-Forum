20099691ef946137960091b0c0778032a9fe9016
Nifazur Rahman <nifazurrahman@Nifazurs-MacBook-Pro.local>
2024-10-19 22:30:01 +0600

    add nav and banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily:{
+        Mulish:['Mulish', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
